69eabaef872ff19b0346fff6cfdb8cd4687bb5ac
Oun <oun.cav@gmail.com>
2025-05-03 15:13:45 +0700

    Add fav and Logo

--- components/Logo/LogoMobile.tsx
+++ components/Logo/LogoMobile.tsx
@@ -2,7 +2,7 @@ const LogoMobile = () => {
     return (
       <>
       <div className="text-[#003366]">
-        <span className="text-3xl md:text-4xl text-[#FFD233]">$</span>Minute
+        <span className="text-3xl md:text-4xl text-[#FFD233]">$</span>Oploan
       </div>
       </>
     );
